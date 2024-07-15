26bf0eef8c039f1f72ae0569526af965adece2e7
zian17 <zian.zcmu120@gmail.com>
2024-07-14 21:10:34 -0600

    nuevos cambios

--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -31,7 +31,15 @@ urlpatterns = [
     path('eliminarComentario/<int:id>/',views_registros.eliminarComentarioContacto,name='Eliminar'),
     path('formEditarComentario/<int:id>/',views_registros.consultarComentarioIndividual, name='ConsultaIndividual'),
     path('editarComentario/<int:id>/',views_registros.editarComentarioContacto,name='Editar'),
-
+    path ('consultas1',views_registros.consultar1,name = "Consultas"),
+    path ('consultas2',views_registros.consultar2,name = "Consultas2"),
+    path ('consultas3',views_registros.consultar3,name = "Consultas3"),
+    path ('consultas4',views_registros.consultar4,name = "Consultas4"),
+    path ('consultas5',views_registros.consultar5,name = "Consultas5"),
+    path ('consultas6',views_registros.consultar6,name = "Consultas6"),
+    path ('consultas7',views_registros.consultar7,name = "Consultas7"),
+    path ('subir',views_registros.archivos,name = "Subir"),
+    
 ]
 
 if settings.DEBUG:
--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,7 +1,10 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Alumnos
 from .models import Comentario
 from .models import ComentarioContacto
+
 
 # Register your models here.
 class AdministrarModelo(admin.ModelAdmin):
@@ -10,6 +13,15 @@ class AdministrarModelo(admin.ModelAdmin):
     search_fields = ('matricula','nombre','carrera','turno')
     date_hierarchy = 'created'
     list_filter = ('carrera','turno')
+    list_per_page = 2
+    list_display_links = ('matricula','nombre')
+    list_editable = ('turno',)
+
+    def get_readonly_fields(self,request, obj: None):
+        if request.user.groups.filter (name="Usuarios").exists():
+            return ('matricula', 'carrera', 'turno')
+        else:
+            return ('created', 'updated')
 
 class AdministrarComentarios(admin.ModelAdmin):
     list_display = ('id', 'coment')
@@ -22,6 +34,8 @@ class AdministrarComentariosContacto(admin.ModelAdmin):
     search_fields = ('id','created')
     date_hierarchy = 'created'
     readonly_fields = ('created', 'id')
+
+
 
 admin.site.register(Alumnos,AdministrarModelo)
 admin.site.register(Comentario, AdministrarComentarios)
